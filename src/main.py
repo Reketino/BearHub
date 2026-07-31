136c100aa8c86c7b62333c914d59c3fe167f29b0
Reketino <bjornevensk8@gmail.com>
2026-07-31 10:45:03 +0200

    feat: added setwindowicon fix: pyside6 QTgui import

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import sys
 from pathlib import Path
 
-from PySide6 import QIcon
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from gui.main_window import MainWindow
 
 from gui.main_window import MainWindow
 
-def get_assets(filename: str) -> Path:
+def get_assets_path(filename: str) -> Path:
     project_root = Path(__file__).resolve().parent.parent
     return project_root / "assets" / filename
 
@@ -16,6 +16,9 @@ def main():
     
     app.setApplicationName("BearHub")
     app.setApplicationDisplayName("Bearhub")
+    app.setWindowIcon(
+        QIcon(str(get_assets_path("bearhub-icon.png")))
+    )
 
     window = MainWindow()
     window.show()
