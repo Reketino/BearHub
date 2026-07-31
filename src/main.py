b56b24f6a561f79373657f8c71e975426638719e
Reketino <bjornevensk8@gmail.com>
2026-07-31 10:24:27 +0200

    feat: added imports in src main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+import sys
+from pathlib import Path
+
+from PySide6 import QIcon
 from PySide6.QtWidgets import QApplication
+from gui.main_window import MainWindow
+
 from gui.main_window import MainWindow
 
 app = QApplication([])
