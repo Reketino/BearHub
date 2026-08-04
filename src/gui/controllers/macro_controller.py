2c2ec8df23695b7c18ac95396574dc474be29cd2
Reketino <bjornevensk8@gmail.com>
2026-08-05 00:01:28 +0200

    feat: implement MacroController for managing macro display and execution

---
+++ src/gui/controllers/macro_controller.py
@@ -0,0 +1,85 @@
+from PySide6.QtCore import QTimer
+
+from gui.macro_dialog import MacroDialog
+from constants.g_keys import G_KEY_MAP
+
+class MacroController:
+    def __init__(self, window):
+        self.view = window
+        
+     #-------- DISPLAY MACROS --------#
+      
+    def display_macros(self, macros):
+        self.view.macros = macros
+        self.view.engine.load_profile(macros)
+        self.view.macro_list.clear()
+            
+        for macro in macros:
+            key_name = G_KEY_MAP.get(
+                macro.input_id,
+                "Unbound"
+            )
+                
+            self.view.macro_list.addItem(
+                f"{macro.name} [{key_name}]"
+            )
+            
+    
+        #-------- SHOW MACRO --------#
+            
+    def show_macro(self, row):
+        if row < 0 or row >= len(self.view.macros):
+            self.view.edit_macro_button.setEnabled(False)
+            self.view.delete_macro_button.setEnabled(False)
+            return
+        
+        self.view.edit_macro_button.setEnabled(True)
+        self.view.delete_macro_button.setEnabled(True)
+        
+        macro = self.view.macros[row]
+        
+        key_name = G_KEY_MAP.get(
+            macro.input_id,
+            "Unbound"
+        )
+        
+        self.view.details.setText(
+            f"Name: {macro.name}\n\n"
+            f"Value:\n{macro.value}\n\n"
+            f"Key: {key_name}\n"
+            f"Preset: {macro.profile_name}\n"
+            f"Device: {macro.device_signature}"
+        )
+        
+        self.view.status.setText(
+            f"Selected {macro.name}"
+        )
+        
+    
+    #-------- EXECUTE SELECTED MACROS --------#
+            
+    def execute_selected_macro(self):
+        row = self.view.macro_list.currentRow()
+            
+        if row < 0:
+                return
+            
+        macro = self.view.macros[row]
+            
+        self.view.status.setText(
+            f"Executing {macro.name} in 2 seconds..."
+        )
+            
+        QTimer.singleShot(
+            2000,
+            lambda: self.execute_macro(macro)
+        )
+        
+    
+    #-------- EXECUTE MACRO --------#
+        
+    def execute_macro(self, macro):
+        self.view.engine.execute_macro(macro)
+        self.view.status.setText(
+            f"Executed {macro.name}"
+        )
