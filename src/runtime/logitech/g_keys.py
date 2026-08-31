16741bc9edf4dac469ffdc8221c7ef06b93621c4
Reketino <bjornevensk8@gmail.com>
2026-09-01 00:02:28 +0200

    feat: add LogitechGKeys class with constructor for HIDppDevice integration

--- src/runtime/logitech/g_keys.py
+++ src/runtime/logitech/g_keys.py
@@ -7,3 +7,13 @@ GKEY_FEATURE_INDEX = 0x10
 
 GKEY_DIVERT_ENABLED = b"\x01"
 GKEY_DIVERT_DISABLED = b"\x00"
+
+class LogitechGKeys:
+    
+    def __init__(
+        self,
+        hidpp: HidppDevice,
+        feature_index: int = GKEY_FEATURE_INDEX
+    ):
+        self.hidpp = hidpp
+        
